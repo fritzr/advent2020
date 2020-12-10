2155d432426f31e2f1eb94bd26d4f46d97d37011
Fritz Reese <fritzoreese@gmail.com>
2020-12-10 18:24:46 -0500

    Day 2 part 2

--- p02/p02.go
+++ p02/p02.go
@@ -11,43 +11,51 @@ import (
 )
 
 type Password struct {
-  min int
-  max int
+  x int
+  y int
   char byte
   password string
 }
 
-func (p *Password) Valid() bool {
+// Part 1 validation
+func (p *Password) P1Valid() bool {
   count := 0
   char := p.char
   for cidx := 0; cidx < len(p.password); cidx++ {
     if char == p.password[cidx] {
       count += 1
-      if count > p.max {
+      if count > p.y {
         return false
       }
     }
   }
-  return count >= p.min
+  return count >= p.x
+}
+
+// Part 2 validation
+func (p *Password) P2Valid() bool {
+  p1 := p.password[p.x - 1] == p.char
+  p2 := p.password[p.y - 1] == p.char
+  return p1 != p2
 }
 
 func parse_spec(spec string) (int, int, error) {
   parts := strings.Split(spec, "-")
   if len(parts) != 2 {
-    return 0, 0, errors.New(fmt.Sprintf("invalid 'min-max' specifier '%s'", spec))
+    return 0, 0, errors.New(fmt.Sprintf("invalid 'X-Y' specifier '%s'", spec))
   }
-  var (min int
-       max int
+  var (x int
+       y int
        err error)
-  min, err = strconv.Atoi(parts[0])
+  x, err = strconv.Atoi(parts[0])
   if err != nil {
     return 0, 0, err
   }
-  max, err = strconv.Atoi(parts[1])
+  y, err = strconv.Atoi(parts[1])
   if err != nil {
-    return min, 0, err
+    return x, 0, err
   }
-  return min, max, nil
+  return x, y, nil
 }
 
 func ParsePasswords(r io.Reader) ([]Password, error) {
@@ -57,7 +65,7 @@ func ParsePasswords(r io.Reader) ([]Password, error) {
   passwords := make([]Password, 1000)
   count := 0
   for scanner.Err() == nil && scanner.Scan() {
-    // min-max char: password (three words)
+    // X-Y char: password (three words)
     spec := scanner.Text()
 
     var char string
@@ -74,12 +82,12 @@ func ParsePasswords(r io.Reader) ([]Password, error) {
       break
     }
 
-    min, max, err := parse_spec(spec)
+    x, y, err := parse_spec(spec)
     if err != nil {
       return passwords, err
     }
 
-    passwords[count] = Password{min, max, char[0], password}
+    passwords[count] = Password{x, y, char[0], password}
     count++
   }
 
@@ -95,25 +103,35 @@ func ParsePasswordsFromFile(path string) ([]Password, error) {
   return ParsePasswords(file)
 }
 
+type password_validator func(*Password) (bool)
+
+func report_valid(passwords []Password, validate password_validator) int {
+  lpass := len(passwords)
+  nvalid := 0
+  for _, password := range passwords {
+    if validate(&password) {
+      nvalid += 1
+    }
+  }
+
+  if nvalid < lpass {
+    fmt.Printf("Oh no! Only %d / %d passwords are valid!\n", nvalid, lpass)
+  } else {
+    fmt.Printf("Yay! All %d passwords are valid!\n", lpass)
+  }
+  return nvalid
+}
+
 func Main(input_path string, verbose bool, args []string) error {
   passwords, err := ParsePasswordsFromFile(input_path)
   if err != nil {
     return err
   }
 
-  nvalid := 0
-  for _, password := range passwords {
-    if password.Valid() {
-      nvalid += 1
-    }
-  }
-
-  lpass := len(passwords)
-  if nvalid < lpass {
-    fmt.Printf("Oh no! Only %d / %d passwords are valid!\n", nvalid, lpass)
-  } else {
-    fmt.Printf("Yay! All %d passwords are valid!\n", lpass)
-  }
+  fmt.Printf("Validation for part 1:\n  ")
+  _ = report_valid(passwords, (*Password).P1Valid)
+  fmt.Printf("Validation for part 2:\n  ")
+  _ = report_valid(passwords, (*Password).P2Valid)
 
   return nil
 }
