6d77b4581a4f41eefdf832dc5603b2f1c2d71b89
Fritz Reese <fritzoreese@gmail.com>
2020-12-14 12:55:54 -0500

    Day 12 part 2

--- p12/p12.go
+++ p12/p12.go
@@ -42,46 +42,121 @@ type Boat struct {
   lat int
   long int
   head int // cardinal heading: 0 is East, 1 is North, etc...
+
+  // Relative position of the waypoint. Used for waypoint travel.
+  wayLat int
+  wayLong int
 }
+
+
 
 // You Must Build A Boat.
-func NewBoat() *Boat {
-  return new(Boat)
+func NewBoat(lat int, long int, head int, wayLat int, wayLong int) *Boat {
+  return &Boat{lat, long, head, wayLat, wayLong}
 }
 
-// Move the boat along a vector (without rotating it).
-func (b *Boat) Move(mag int, head int) {
+// Reset the position of the boat.
+func (b *Boat) Set(lat int, long int) {
+  b.lat = lat
+  b.long = long
+}
+
+func head2Coord(head int) (lat int, long int) {
   if head % 2 == 1 { // N=1, S=3 => 1, -1
     // Positive latitude is in the north heading.
-    latDelta := 2 - head
-    b.lat = b.lat + (mag * latDelta)
+    lat = 2 - head
   } else { // {E=0, W=2} => 1, -1
     // Positive longitude is in the east heading.
-    longDelta := 1 - head
-    b.long = b.long + (mag * longDelta)
+    long = 1 - head
   }
+  return lat, long
 }
 
-// Rotate the boat by adjusting the heading.
-func (b *Boat) Rotate(headDelta int) {
-  b.head += headDelta
-  b.head = b.head % len(Headings)
-  if b.head < 0 {
-    b.head += len(Headings)
-  }
+// Move a point along a vector given in magnitude and heading.
+func move(lat int, long int, mag int, head int) (int, int) {
+  latDelta, longDelta := head2Coord(head)
+  lat += mag * latDelta
+  long += mag * longDelta
+  return lat, long
 }
+
+// Move a point component-wise.
+func moveTo(lat int, long int, latDelta int, longDelta int) (int, int) {
+  lat += latDelta
+  long += longDelta
+  return lat, long
+}
+
+// Rotate a heading by a number of units (CCW 90 degree rotations).
+func rotate(head int, headDelta int) int {
+  head += headDelta
+  head = head % len(Headings)
+  if head < 0 {
+    head += len(Headings)
+  }
+  return head
+}
+
+// Rotate a point a number of units (CCW 90 degree rotations) about the origin.
+//
+// headDelta must be one of {0, 1, 2, 3} for no rotation, 90CCW, 180, 90CW.
+func rotatePoint(lat int, long int, headDelta int) (int, int) {
+  // Swap for odd rotations (+/-90)
+  if headDelta % 2 != 0 {
+    tmpLat := lat
+    lat = long
+    long = tmpLat
+  }
+
+  // Negate latitude for -90, 180
+  // lat *= -1 * ((headDelta / 2) * 2 - 1)
+  if headDelta == 2 || headDelta == 3 {
+    lat *= -1
+  }
+
+  // Negate longitude for 90, 180
+  // long *= -1 * ((((headDelta + 1) / 2) % 2) * 2 - 1)
+  if headDelta == 1 || headDelta == 2 {
+    long *= -1
+  }
+
+  return lat, long
+
+  /*
+  switch headDelta {
+  // Rotate CCW: swap and negate longitude
+  case 1:
+    lat := b.wayLat
+    b.wayLat = b.wayLong
+    b.wayLong = -lat
+  // Flip: don't swap... negate both
+  case 2:
+    b.wayLat = -b.wayLat
+    b.wayLong = -b.wayLong
+  // Rotate CW: swap and negate latitude
+  case 3:
+    lat := b.wayLat
+    b.wayLat = -b.wayLong
+    b.wayLong = lat
+  }
+  */
+}
+
 
 // Follow a Direction.
-func (b *Boat) MoveDirection(d Direction) {
+func (b *Boat) Move(d Direction) {
   heading := Headings[d.action]
   if heading != 0 {
-    b.Move(d.value, heading - 1)
+    // Move the waypoint with magnitude and heading.
+    b.lat, b.long = move(b.lat, b.long, d.value, heading - 1)
   } else {
+    // Rotate the boat (adjust the heading).
     rotation := Rotations[d.action]
     if rotation != 0 {
-      b.Rotate(rotation * (d.value / 90))
+      b.head = rotate(b.head, rotation * (d.value / 90))
     } else {
-      b.Move(d.value, b.head)
+      // Move the boat along its current heading.
+      b.lat, b.long = move(b.lat, b.long, d.value, b.head)
     }
   }
 }
@@ -89,7 +164,34 @@ func (b *Boat) MoveDirection(d Direction) {
 // Follow several Directions.
 func (b *Boat) Follow(directions []Direction) {
   for _, d := range directions {
-    b.MoveDirection(d)
+    b.Move(d)
+  }
+}
+
+// Move the waypoint (or move the boat to the waypoint).
+func (b *Boat) MoveWaypoint(w Direction) {
+  heading := Headings[w.action]
+  if heading != 0 {
+    // Move the waypoint with magnitude and heading.
+    b.wayLat, b.wayLong = move(b.wayLat, b.wayLong, w.value, heading - 1)
+  } else {
+    rotation := Rotations[w.action]
+    if rotation != 0 {
+      // Rotate the waypoint about the boat N degrees.
+      headDelta := rotate(0, (rotation * (w.value / 90)))
+      b.wayLat, b.wayLong = rotatePoint(b.wayLat, b.wayLong, headDelta)
+    } else {
+      // Move to the waypoint N times.
+      b.lat, b.long = moveTo(b.lat, b.long,
+        b.wayLat * w.value, b.wayLong * w.value)
+    }
+  }
+}
+
+// Follow directions which modify the waypoint.
+func (b *Boat) FollowWaypoint(directions []Direction) {
+  for _, d := range directions {
+    b.MoveWaypoint(d)
   }
 }
 
@@ -153,16 +255,27 @@ func ReadDirectionsFromFile(path string) ([]Direction, error) {
   return ReadDirections(file)
 }
 
+func printBoat(boat *Boat, fromLat int, fromLong int) {
+  fmt.Println("New position and heading:", boat.Str())
+  fmt.Printf("Manhattan distance from start: %d\n",
+    boat.L1Distance(fromLat, fromLong))
+}
+
 func Main(input_path string, verbose bool, args []string) error {
   directions, err := ReadDirectionsFromFile(input_path)
   if err != nil {
     return err
   }
 
-  boat := NewBoat()
+  // Part 1 -- follow directions using turtle mechanics.
+  boat := NewBoat(/*pos:*/ 0, 0, /*head:*/0/*E*/, /*waypoint:*/ 1/*N*/, 10/*E*/)
   boat.Follow(directions)
-  fmt.Println("New position and heading:", boat.Str())
-  fmt.Printf("Manhattan distance from start: %d\n", boat.L1Distance(0, 0))
+  printBoat(boat, 0, 0)
+
+  // Part 2 -- follow directions using waypoint mechanics.
+  boat.Set(0, 0)
+  boat.FollowWaypoint(directions)
+  printBoat(boat, 0, 0)
 
   return nil
 }
