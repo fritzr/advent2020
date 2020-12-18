c6bd2dbcb7cfccb1218fc1d4dfc961da68b2cd3a
Fritz Reese <fritzoreese@gmail.com>
2020-12-18 09:15:44 -0500

    Day 13 part 2 is correct...  but still iterative

--- p13/p13.go
+++ p13/p13.go
@@ -10,6 +10,8 @@ import (
   "strings"
 )
 
+var gVerbose = false
+
 type BusSchedule struct {
   // List of bus IDs in the same order as the input.
   buses []int
@@ -34,17 +36,113 @@ func NewBusSchedule(sched string) (*BusSchedule, error) {
   return b, nil
 }
 
-func (b *BusSchedule) NextAvailable(time int) map[int]int {
-  nextAvailable := make(map[int]int, len(b.buses))
+func (b *BusSchedule) NextAvailable(time int64) map[int]int64 {
+  nextAvailable := make(map[int]int64, len(b.buses))
   for _, busId := range b.buses {
-    nextAvailable[busId] = busId * (1 + time / busId)
+    nextAvailable[busId] = int64(busId) * (1 + time / int64(busId))
   }
   return nextAvailable
 }
 
-func (b *BusSchedule) ConstrainedTime() int {
-  // Find the time t for which bus x departs at t + busOffsets[x].
-  // TODO
+// Compute the parameter T for integer n and m such that:
+//   A * n = T and = B * m = T + O.
+//
+// In other words,
+//   A * n = B * m - O.
+//
+// This function returns n. To obtain T itself, multiply the result by A.
+func offsetMatchN(A int64, B int64, O int64) int64 {
+  Q := O / A
+  O = O % A
+
+  // In the degenerate case where the desired offset is zero,
+  // we can simply use n = B and m = A such that A * B = T = B * A.
+  // Of course, with a non-zero offset divisible by A, we still need to
+  // subtract by the quotient O / A.
+  if O == 0 {
+    return B - Q
+  }
+
+  // First find the quotient q and remainder r w.r.t B.
+  // We will further decompose A * n into k * (B + r).
+  q := B / A
+  r := B % A
+
+  // Find a = k * r = O mod A, the first multiple of r congruent to O modulo A.
+  // XXX can we do this mathematically or must we iterate?
+  k := int64(1)
+  a := r
+  for (a % A) != O {
+    a += r
+    k += 1
+  }
+
+  // Multiply the result by the original quotient of B / A,
+  // then add (a / A) to obtain the first such number divisible by A.
+
+  // Note that we tracked a = r * k already.
+  // The formula for n is n = (B / A) * k + ((r * k) / A).
+  // In other words, A * n = k * (B + r) = B * m + O such that k * r = O mod A.
+  // We also need to subtract Q = O / A in case the requested offset is
+  // larer than the root A.
+  n := q * k + (a / A) - Q
+
+  // Now one could compute m from the definition simply as:
+  // m := (A * n + O) / B
+  return n
+}
+
+func (b *BusSchedule) ConstrainedTime() int64 {
+  lcm := int64(b.buses[0])
+  t := int64(0)
+
+  // Find the sub-result t for A and B which defines the sequence:
+  //
+  //   { t, t + lcm(A, B), ..., t + lcm(A, B) * n }
+  //
+  for _, busId := range b.buses[1:] {
+    if gVerbose {
+      fmt.Printf("  %d + %d * n = T\n", t, lcm )
+      fmt.Printf("  %d + %d * n = T\n", t + int64(b.busOffsets[busId]), busId)
+      fmt.Printf("===================\n")
+    }
+    t += lcm * offsetMatchN(lcm, int64(busId), t + int64(b.busOffsets[busId]))
+    lcm *= int64(busId)
+  }
+
+  return t
+}
+
+func (b *BusSchedule) ConstrainedTimeBruteForce(maxIterations int64) int64 {
+  // Find the time t for which bus x departs at t + busOffsets[x] (for all x).
+  bus0 := int64(b.buses[0])
+  t := bus0
+  n := int64(1)
+  for n < maxIterations + 1 {
+    valid := true
+    /*
+    if n == int64(152683) {
+      fmt.Printf("%d: time=%d\n", n, t)
+    }
+    */
+    for id, time := range b.NextAvailable(t) {
+      /*
+      if n == int64(152683) {
+        fmt.Printf("    %d requires offset %d, arrives at offset %d (%d)\n",
+          id, b.busOffsets[id], time - t, time)
+      }
+      */
+      if int64(id) != bus0 && (time - t) != int64(b.busOffsets[id]) {
+        valid = false
+        break
+      }
+    }
+    if valid {
+      return t
+    }
+    n++
+    t += bus0
+  }
   return -1
 }
 
@@ -73,20 +171,22 @@ func ReadScheduleFromFile(path string) (int, *BusSchedule, error) {
 }
 
 func Main(input_path string, verbose bool, args []string) error {
+  gVerbose = verbose
   timestamp, schedule, err := ReadScheduleFromFile(input_path)
   if err != nil {
     return err
   }
 
   // Part 1: find the earliest bus after the given timestamp.
+  time := int64(timestamp)
   earliestBus := -1
-  earliestWaitTime := -1
-  nextAvailable := schedule.NextAvailable(timestamp)
+  earliestWaitTime := int64(-1)
+  nextAvailable := schedule.NextAvailable(time)
   for nextBus, nextTime := range nextAvailable {
     if verbose {
       fmt.Printf("  %d arrives next at %d\n", nextBus, nextTime)
     }
-    wait := nextTime - timestamp
+    wait := nextTime - time
     if earliestWaitTime < 0 || wait < earliestWaitTime {
       earliestBus = nextBus
       earliestWaitTime = wait
@@ -95,13 +195,12 @@ func Main(input_path string, verbose bool, args []string) error {
 
   earliestBusTime := nextAvailable[earliestBus]
   fmt.Printf("Next bus after %d is %d, arriving at %d (wait time %d).\n",
-    timestamp, earliestBus, earliestBusTime, earliestWaitTime)
+    time, earliestBus, earliestBusTime, earliestWaitTime)
   fmt.Printf("    %d x %d = %d\n",
-    earliestBus, earliestWaitTime, earliestBus*earliestWaitTime)
+    earliestBus, earliestWaitTime, int64(earliestBus)*earliestWaitTime)
 
-  // Part 2: find timestamp which matches the schedule.
+  // Part 2: find timestamp which matches the scheduled wait times.
   constrainedTime := schedule.ConstrainedTime()
   fmt.Printf("Schedule matches constraints starting at %d.\n", constrainedTime)
-
   return nil
 }
